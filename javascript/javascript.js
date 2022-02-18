2ebc8c05544c50baeb9f68e2e8f6b798279a4b06
oliverpaulsson <oliverpaulsson1@gmail.com>
2022-02-18 18:17:14 +0100

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,91 @@
+// ANIMATE ON SCROLL
+
+(function() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('fade-in-element');
+          element.classList.remove('hidden');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  })();
+
+// HEADER AND TOP ARROW ON SCROLL
+
+jQuery(window).on('scroll', function() {
+    if(jQuery(window).scrollTop() > 50) {
+        jQuery('.header').css('background-color', '#000000');
+    } else {
+       jQuery('.header').css('background-color', 'transparent');
+    }
+});
+
+jQuery(window).on('scroll', function() {
+    if(jQuery(window).scrollTop() > 50) {
+        jQuery('#uparrow').css('opacity', '1');
+    } else {
+       jQuery('#uparrow').css('opacity', '0');
+    }
+});
+
+// SMOOTH SCROLLING
+
+$("#arrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#omoss").offset().top - 70
+    }, 1500);
+});
+
+$("#uparrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hero").offset().top - 70
+    }, 1500);
+});
+
+$(".omosslink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#omoss").offset().top - 70
+    }, 1500);
+});
+
+$(".serviceslink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top - 70
+    }, 1500);
+});
+
+$(".gallerylink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#gallery").offset().top - 70
+    }, 1500);
+});
+
+$(".clientslink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#clients").offset().top - 70
+    }, 1500);
+});
+
+$(".contactlink").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top - 70
+    }, 1500);
+});
